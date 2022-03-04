39b8929bfd36072810ce3c20d96b31b5f1472a73
GroupProjectt2 <97778611+GroupProjectt2@users.noreply.github.com>
2022-03-04 20:49:34 +0530

    updated as module

--- admin.py
+++ admin.py
@@ -4,8 +4,6 @@ cnx = ms.connect(
     user = 'root',
     password = '12345',
     database = 'jalshaabh')
-if cnx.is_connected():
-    print("Successfully connected!")
 
 cur = cnx.cursor()
 
@@ -14,22 +12,22 @@ def change_officer_name(officer_name,officer_id):
     query="UPDATE financial,officer SET financial.name_officer = '{}', officer.name_officer = '{}' WHERE financial.id_officer = '{}' AND officer.id_officer = '{}'".format(officer_name,officer_name,officer_id,officer_id)
     cur.execute(query)
     cnx.commit()
-    print("Successfully updated")
+    return "Successfully updated"
 
 def Update_grant(officer_id,new_grant):
     query="UPDATE financial SET m_given = {} WHERE id_officer = '{}'".format(new_grant,officer_id)
     cur.execute(query)
     cnx.commit()
-    print("Successfully updated")
+    return "Successfully updated"
     
 def Update_salary(officer_id,new_salary):
     query="UPDATE financial SET m_salary = {} WHERE id_officer = '{}'".format(new_salary,officer_id)
     cur.execute(query)
     cnx.commit()
-    print("Successfully updated")
+    return "Successfully updated"
 
 def Update_pincode(old_pincode,new_pincode):
     query="UPDATE geography, officer SET geography.pincode_area = '{}',officer.pincode_area = '{}' WHERE geography.pincode_area = '{}' AND officer.pincode_area = '{}'".format(new_pincode,new_pincode,old_pincode,old_pincode)
     cur.execute(query)
     cnx.commit()
-    print("Successfully updated")
+    return "Successfully updated"
