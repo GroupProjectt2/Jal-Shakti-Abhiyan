82c3e5408781b4daab35380272ba117dc1940e15
GroupProjectt2 <97778611+GroupProjectt2@users.noreply.github.com>
2022-03-04 20:46:49 +0530

    updated as module

--- action.py
+++ action.py
@@ -1,20 +1,6 @@
 import mysql.connector as ms
 import random
 
-def action_advice(diff):
-    advice = ['Spread awareness among the resident population about water conservation',
-              'Develop watersheds',
-              'Set up more rainwater harvesting locations',
-              'Plant trees around the area',
-              'Renovate existing water bodies/tanks',]
-    if diff > 0:
-        print('\nNot enough water is available in the region.\nConsider taking the following actions:\n')
-        print(advice[random.randrange(0,len(advice))])
-        return
-    else:
-        print('\nAdequate water is available in the region.\nGreat work!')
-        return
-
 cnx = ms.connect(
     host = 'localhost',
     user = 'root',
@@ -22,29 +8,29 @@ cnx = ms.connect(
     database = 'jalshaabh')
 cur = cnx.cursor()
 
-action_select = '''
-SELECT pincode_area,
-       water_available, 
-       water_required 
-       FROM Geography'''
+def action_advice(inp):
+    
+    action_select = '''SELECT pincode_area, water_available, water_required FROM Geography'''
 
-cur.execute(action_select)
+    cur.execute(action_select)
 
-data = cur.fetchall()
+    data = cur.fetchall()
 
-cont = True
-while cont == True:
-    inp = int(input('\nPlease enter the pincode of an area:\t'))
     found = False
     for row in data:
         if int(row[0]) == inp:
             found = True
-            water_diff = int(row[2])-int(row[1])
-            action_advice(water_diff)
+            diff = int(row[2])-int(row[1])
     if found == False:
-        print('\nThat is an invalid pincode!')
-    cont = str(input('\nDo you want to continue? (y/n) :\t'))
-    if cont.lower() == 'n':
-        cont = False
+        return 'That is an invalid pincode!'
+        
+    advice = ['Spread awareness among the resident population about water conservation',
+              'Develop watersheds',
+              'Set up more rainwater harvesting locations',
+              'Plant trees around the area',
+              'Renovate existing water bodies/tanks',]
+    if diff > 0:
+        adv = advice[random.randrange(0,len(advice))]
+        return adv
     else:
-        cont = True
+        return '\nAdequate water is available in the region.\nGreat work!'
