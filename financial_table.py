fbe209d5eb69c6dde6550ee8664d5f57e4ea4c46
GroupProjectt2 <97778611+GroupProjectt2@users.noreply.github.com>
2022-03-08 15:50:06 +0530

    final update

--- financial_table.py
+++ financial_table.py
@@ -2,7 +2,7 @@ import mysql.connector as ms
 cnx = ms.connect(
     host = 'localhost',
     user = 'root',
-    password = '12345',
+    password = 'RInger@123',
     database = 'jalshaabh')
 
 cur = cnx.cursor()
@@ -12,31 +12,19 @@ def Display_officerID(officer_id):
     query="SELECT * FROM financial WHERE id_officer = '{}'".format(officer_id)
     cur.execute(query)
     result=cur.fetchall()
-    return result
+    return result[0]
 
 def Display_grant(officer_id):
     query="SELECT m_given FROM financial WHERE id_officer = '{}'".format(officer_id)
     cur.execute(query)
     result=cur.fetchone()
     return "Grant for this area is Rs '{}'".format(result[0])
-    
-def Update_grant(officer_id,new_grant):
-    query="UPDATE financial SET m_given = {} WHERE id_officer = '{}'".format(new_grant,officer_id)
-    cur.execute(query)
-    cnx.commit()
-    return "Successfully updated"
 
 def Display_salary(officer_id):
     query="SELECT m_salary FROM financial WHERE id_officer = '{}'".format(officer_id)
     cur.execute(query)
     result=cur.fetchone()
     return "Salary for this area is Rs '{}'".format(result[0])
-
-def Update_salary(officer_id,new_salary):
-    query="UPDATE financial SET m_salary = {} WHERE id_officer = '{}'".format(new_salary,officer_id)
-    cur.execute(query)
-    cnx.commit()
-    return "Successfully updated"
     
 def Display_cost(officer_id):
     query="SELECT m_oper FROM financial WHERE id_officer = '{}'".format(officer_id)
