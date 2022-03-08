fe72acfd07d92dbbe4f88ae8191690715e3411c3
GroupProjectt2 <97778611+GroupProjectt2@users.noreply.github.com>
2022-03-08 15:51:18 +0530

    final update

--- geographic_table.py
+++ geographic_table.py
@@ -12,28 +12,28 @@ def SearchByPincode(pin):
     querysbp = 'SELECT * FROM Geography WHERE pincode_area = "{}"'.format(pin)
     cur.execute(querysbp)
     datasbp = cur.fetchall()
-    return datasbp
+    return datasbp[0]
 
 def Display_Rainfall(pin):
     querydr = 'SELECT water_rain FROM Geography WHERE pincode_area = "{}"'.format(pin)
     cur.execute(querydr)
-    datadr = cur.fetchall()
-    return datadr
+    datadr = cur.fetchone()
+    return "Annual Rainfall in this area is '{}' mm".format(datadr[0])
 
 def Display_Ground_Water(pin):
     querydgw = 'SELECT water_ground FROM Geography WHERE pincode_area = "{}"'.format(pin)
     cur.execute(querydgw)
-    datadgw = cur.fetchall()
-    return datadgw
+    datadgw = cur.fetchone()
+    return "Ground water level in this area is '{}' m/bgl".format(datadgw[0])
 
 def Display_Total_Water(pin):
     querydtw = 'SELECT water_available FROM Geography WHERE pincode_area = "{}"'.format(pin)
     cur.execute(querydtw)
-    datadtw = cur.fetchall()
-    return datadtw
+    datadtw = cur.fetchone()
+    return "Total useable water in this area is '{}' KL".format(datadtw[0])
 
 def Display_Required_Water(pin):
     querydrw = 'SELECT water_required FROM Geography WHERE pincode_area = "{}"'.format(pin)
     cur.execute(querydrw)
-    datadrw = cur.fetchall()
-    return datadrw
+    datadrw = cur.fetchone()
+    return "Total useable water required in this is '{}' KL".format(datadrw[0])
