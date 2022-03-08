c6f26af623520cc8de21030b3894dd42cd57096a
GroupProjectt2 <97778611+GroupProjectt2@users.noreply.github.com>
2022-03-08 15:13:39 +0530

    all buttons finished, beautify a bit pls

--- screen3.py
+++ screen3.py
@@ -1,12 +1,11 @@
 from tkinter import *
 import screen2
 import geographic_table as geo
-import matplotlib
 def Screen3():
     
     root = Tk()
     root.title('Geographic')
-    root.geometry('450x150')
+    root.geometry('650x400')
     root.configure(bg='#f9bf8f')
 
 
@@ -18,15 +17,46 @@ def Screen3():
 
     inp1.grid(row=0,column=1)
 
-    def Onclick():
+    def Onclick1():
         pin=inp1.get()
-        #print(pin)
         print(geo.SearchByPincode(pin))
+
+    def Onclick2():
+        pin=inp1.get()
+        for x in geo.Display_Rainfall(pin):
+            print(x)
+
+    def Onclick3():
+        pin=inp1.get()
+        for x in geo.Display_Ground_Water(pin):
+            print(x)
+
+    def Onclick4():
+        pin=inp1.get()
+        for x in geo.Display_Total_Water(pin):
+            print(x)
+
+    def Onclick5():
+        pin=inp1.get()
+        for x in geo.Display_Required_Water(pin):
+            print(x)
         
-    Enter=Button(root,text="ENTER",bg="white",command=Onclick)
+    Enter=Button(root,text="Search by Pincode",bg="white",command=Onclick1)
     Enter.grid(row=1,column=1,pady=10,ipadx=20,ipady=10)
 
+    Enter=Button(root,text="Display Rainfall",bg="white",command=Onclick2)
+    Enter.grid(row=2,column=1,pady=10,ipadx=20,ipady=10)
+
+    Enter=Button(root,text="Display Groundwater",bg="white",command=Onclick3)
+    Enter.grid(row=2,column=2,pady=10,ipadx=20,ipady=10)
+
+    Enter=Button(root,text="Display Total Water",bg="white",command=Onclick4)
+    Enter.grid(row=3,column=1,pady=10,ipadx=20,ipady=10)
+
+    Enter=Button(root,text="Display Required Water",bg="white",command=Onclick5)
+    Enter.grid(row=3,column=2,pady=10,ipadx=20,ipady=10)
+
     Back=Button(root,text="<- Main Menu",bg="yellow",command=lambda: [root.destroy(),screen2.Screen2()])
-    Back.grid(row=2,column=3,ipadx=15,ipady=0.5)
+    Back.grid(row=4,column=3,ipadx=15,ipady=0.5)
 
     root.mainloop()
